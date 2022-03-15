6dbb43cd7a6de7af8c2f500f701bbaf33e80a71a
NianHung <joanne.lee@iss.nthu.edu.tw>
2022-03-15 16:09:07 +0800

    finish part A

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -7,5 +7,8 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    sum_digits = nums_a.reverse.each_with_index.map { |digit, index| index.odd? ? digit * 2 : digit }
+    sum_digits = sum_digits.map { |digit| digit > 9 ? digit - 9 : digit }
+    (sum_digits.inject(0, :+) % 10).zero?
   end
 end
