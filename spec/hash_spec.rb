bd08794f7227bc32714506ae80ac079f2c806dab
blauwaliu <jeanliu0320@gapp.nthu.edu.tw>
2022-03-22 13:52:29 +0800

    Write spec for hash part 1.

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../credit_card'
 require 'minitest/autorun'
 
@@ -25,10 +27,15 @@ describe 'Test hashing requirements' do
   describe 'Test regular hashing' do
     describe 'Check hashes are consistently produced' do
       # TODO: Check that each card produces the same hash if hashed repeatedly
+      cards_hash_1 = cards.map(&:hash)
+      cards_hash_2 = cards.map(&:hash)
+      _(cards_hash_1).must_equal cards_hash_2
     end
 
     describe 'Check for unique hashes' do
       # TODO: Check that each card produces a different hash than other cards
+      cards_hash = cards.map(&:hash)
+      _(cards_hash.uniq).must_equal cards_hash.length
     end
   end
 
